c3dcf64e382cadd9e638638a29e12edeb2136706
deepaklumia <deepakkumar12071998@gmail.com>
2022-10-15 00:32:40 +0530

    calling all function in router file

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,24 +1,22 @@
-const express = require('express');
-const router = express.Router();///test-you
+const express = require("express");
+const router = express.Router(); ///test-you
 //importing a custom module
-const xyz = require('../logger')
-//importing external package
-const underscore = require('underscore')
 
-router.get('/test-me', function (req, res) {
-    //Calling the components of a different custom module
-    console.log("Calling my function ",xyz.myFunction())
-    console.log("The value of the constant is ",xyz.myUrl)
-    //Trying to use an external package called underscore
-    let myArray = ['Akash', 'Pritesh', 'Sabiha']
-    let result = underscore.first(myArray)
-    console.log("The result of underscores examples api is : ", result)
-    
-    res.send('My first ever api!')
+const logger = require("../logger/logger");
+//importing external package
+// const underscore = require('underscore')
+const helper = require("../util/helper");
+const formatter = require("../validator/formatter");
+const lodash = require("../lodash/useLodash");
+router.get("/test-me", function (req, res) {
+    logger.welcome();
+    helper.detailsOfBatch();
+    formatter.trim("  FunctionUp ");
+    lodash.monthOfYear();
+    res.send("My first ever api!");
 
     //To be tried what happens if we send multiple response
     //res.send('My second api!')
 });
 
 module.exports = router;
-
