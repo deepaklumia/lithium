8324181fd9c1007fa426bbda2e3aad37e5c61b32
deepaklumia <deepakkumar12071998@gmail.com>
2022-10-15 00:35:52 +0530

    detail of batches of functionUp

---
+++ src/logger/logger.js
@@ -0,0 +1,9 @@
+const url = "https://www.google.com";
+
+let printSomething = function () {
+    console.log(
+        "Welcome to my application. I am Deepak Kumar and a part of FunctionUp Plutonium cohort."
+    );
+};
+
+module.exports.welcome = printSomething;
