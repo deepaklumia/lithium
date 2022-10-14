eb0b7b8ccbcea59824cf4e99923c64b00fbe56aa
deepaklumia <deepakkumar12071998@gmail.com>
2022-10-14 14:53:33 +0530

    add logger file

---
+++ src/routes/logger.js
@@ -0,0 +1,7 @@
+const url = "http://google.com";
+let myFunctuation=function(){
+      console.log("my function run ");
+      
+}
+module.exports.myUrl=url;
+module.exports.welcome=myFunctuation();
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router();
+const logger = require('./logger');
 
 router.get('/test-me', function (req, res) {
     console.log('My batch is', abc.name)
